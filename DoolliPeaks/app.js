7c8e20342d86c3ca7e3f4a9a91dce50b20f323af
Mike Kozelsky <mike.kozelsky@gmail.com>
2014-02-12 14:44:02 -0500

    can filter

--- DoolliPeaks/app.js
+++ DoolliPeaks/app.js
@@ -4,16 +4,18 @@ var peaks = angular.module('peaks', ['ngResource']);
 
 
 peaks.factory('Databases', ['$resource', function ($resource) {
-  return $resource('http://devapi.doolli.com:8080/devapi/databases/:dbId');
+  return $resource('http://devapi.doolli.com:8080/devapi/databases/:db_id');
 }]);
 
 peaks.factory('FieldMap', [function () {
   return {
+    //what i call it : what doolli calls it
     id: null,
     rank: 'Rank',
     mountain: 'Mountain',
     elevation: 'Elevation (feet)',
-    date: 'Ascent Date'
+    date: 'Ascent Date',
+    companions: 'Companions'
   };
 }]);
 
@@ -38,7 +40,14 @@ peaks.controller('PeaksCtrl', ['$scope', 'Databases', 'FieldMap', function ($sco
     $scope.lastSorter = sorter;
     var sortField = FieldMap[sorter];
 
-    Databases.get({ dbId: 2776, application_key: '4D871AF7-8D27-11E3-94B8-22000A8B101E', count: 50, sort_field_name: sortField, sort_by: $scope.sortBy }, function (dbData) {
+    Databases.get({
+      db_id: 2801,
+      application_key: '4D871AF7-8D27-11E3-94B8-22000A8B101E',
+      count: 50,
+      sort_field_name: sortField,
+      sort_by: $scope.sortBy,
+      field_filters: !$scope.currentFilter ? null : JSON.stringify([$scope.currentFilter])
+    }, function (dbData) {
       $scope.dbData = dbData;
       var items = [];
       for (var i = 0; i < dbData.items.length; i++) {
@@ -48,12 +57,19 @@ peaks.controller('PeaksCtrl', ['$scope', 'Databases', 'FieldMap', function ($sco
           rank: _.find(item.fields, function (field) { return field.field_name === FieldMap.rank; }).field_values[0],
           mountain: _.find(item.fields, function (field) { return field.field_name === FieldMap.mountain; }).field_values[0],
           elevation: _.find(item.fields, function (field) { return field.field_name === FieldMap.elevation; }).field_values[0],
-          date: _.find(item.fields, function (field) { return field.field_name === FieldMap.date; }).field_values[0]
+          date: _.find(item.fields, function (field) { return field.field_name === FieldMap.date; }).field_values[0],
+          companions: _.find(item.fields, function (field) { return field.field_name === FieldMap.companions; }).field_values
         });
       }
 
       $scope.peaks = items;
     });
+  };
+
+  $scope.setCompanionFilter = function (companion) {
+    $scope.currentFilter = { field_name: FieldMap.companions, field_value: companion };
+
+    $scope.fetchBy();
   };
 
   $scope.fetchBy();
