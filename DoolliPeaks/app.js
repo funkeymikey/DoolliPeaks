0faaa0b965ec318f30063ac009d2a95f68007542
Mike Kozelsky <mike.kozelsky@gmail.com>
2014-02-18 15:01:57 -0500

    it didn't work with updates.  Fix'd

--- DoolliPeaks/app.js
+++ DoolliPeaks/app.js
@@ -8,9 +8,8 @@ peaks.factory('Databases', ['$resource', function ($resource) {
   //return $resource('http://localhost:8080/devapi/databases/:db_id');
 }]);
 
-peaks.controller('PeaksCtrl', ['$scope', 'Databases', 'FieldMap', function ($scope, Databases, FieldMap) {
+peaks.controller('PeaksCtrl', ['$scope', 'Databases', function ($scope, Databases) {
 
-  $scope.lastSorter = 'id';
 
   //gets the data, ordered by the specified field
   $scope.fetchBy = function (sorter) {
@@ -27,24 +26,24 @@ peaks.controller('PeaksCtrl', ['$scope', 'Databases', 'FieldMap', function ($sco
     }
 
     $scope.lastSorter = sorter;
-    var sortField = FieldMap[sorter];
+    
 
     Databases.get({
       db_id: 2801,
       application_key: '4D871AF7-8D27-11E3-94B8-22000A8B101E',
       count: 50,
-      sort_field_name: sortField,
+      field_filters: !$scope.currentFilter ? null : JSON.stringify([$scope.currentFilter]),
       sort_by: $scope.sortBy,
-      field_filters: !$scope.currentFilter ? null : JSON.stringify([$scope.currentFilter])
+      sort_field_name: $scope.lastSorter
     }, function (dbData) {
       $scope.dbData = dbData;
 
       var fieldMap = {
-        rank: _.find(dbData.fields, function (field) { return field.field_name === FieldMap.rank; }).field_id,
-        mountain: _.find(dbData.fields, function (field) { return field.field_name === FieldMap.mountain; }).field_id,
-        elevation: _.find(dbData.fields, function (field) { return field.field_name === FieldMap.elevation; }).field_id,
-        date: _.find(dbData.fields, function (field) { return field.field_name === FieldMap.date; }).field_id,
-        companions: _.find(dbData.fields, function (field) { return field.field_name === FieldMap.companions; }).field_id
+        rank: _.find(dbData.fields, function (field) { return field.field_name === 'Rank'; }).field_id,
+        mountain: _.find(dbData.fields, function (field) { return field.field_name === 'Mountain'; }).field_id,
+        elevation: _.find(dbData.fields, function (field) { return field.field_name === 'Elevation (feet)'; }).field_id,
+        date: _.find(dbData.fields, function (field) { return field.field_name === 'Ascent Date'; }).field_id,
+        companions: _.find(dbData.fields, function (field) { return field.field_name === 'Companions'; }).field_id
       };
 
 
@@ -52,7 +51,7 @@ peaks.controller('PeaksCtrl', ['$scope', 'Databases', 'FieldMap', function ($sco
       for (var i = 0; i < dbData.items.length; i++) {
         var item = dbData.items[i].field_values;
         items.push({
-          itemId: item.content_item_id,
+          itemId: dbData.items[i].content_item_id,
           rank: item[fieldMap.rank][0],
           mountain: item[fieldMap.mountain][0],
           elevation: item[fieldMap.elevation][0],
@@ -66,11 +65,12 @@ peaks.controller('PeaksCtrl', ['$scope', 'Databases', 'FieldMap', function ($sco
   };
 
   $scope.setCompanionFilter = function (companion) {
-    $scope.currentFilter = { field_name: FieldMap.companions, field_value: companion };
+    $scope.currentFilter = { field_name: 'Companions', field_value: companion };
 
     $scope.fetchBy();
   };
 
+  $scope.lastSorter = null;
   $scope.fetchBy();
 
 }]);
